6635b525314ad912acab5871d0700db2c25963bf
Sander van Leeuwen <replytosander@gmail.com>
2012-10-04 15:02:39 +0200

    added javascript needed for inline content to widget. this way there's no need for overriding the admin templates. tested in django 1.3

--- sortedm2m/forms.py
+++ sortedm2m/forms.py
@@ -10,12 +10,12 @@ from django.forms.models import ModelMultipleChoiceField
 from django.utils.encoding import force_unicode
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
 
 
 STATIC_URL = getattr(settings, 'STATIC_URL', settings.MEDIA_URL)
 
 class SortedFilteredSelectMultiple(forms.SelectMultiple):
-
     """
     A SortableSelectMultiple with a JavaScript filter interface.
 
@@ -29,8 +29,8 @@ class SortedFilteredSelectMultiple(forms.SelectMultiple):
 
     class Media:
         js = (settings.ADMIN_MEDIA_PREFIX + "js/core.js",
-          STATIC_URL + "sortedm2m/OrderedSelectBox.js",
-          STATIC_URL + "sortedm2m/OrderedSelectFilter.js")
+              STATIC_URL + "sortedm2m/OrderedSelectBox.js",
+              STATIC_URL + "sortedm2m/OrderedSelectFilter.js")
 
     def build_attrs(self, attrs=None, **kwargs):
         attrs = super(SortedFilteredSelectMultiple, self).\
@@ -53,8 +53,45 @@ class SortedFilteredSelectMultiple(forms.SelectMultiple):
         output.append(u'<script>addEvent(window, "load", function(e) {')
         output.append(u'OrderedSelectFilter.init("id_%s", "%s", %s, "%s") });</script>\n' %\
                       (name, name.split('-')[-1], int(self.is_stacked), settings.ADMIN_MEDIA_PREFIX))
-        return mark_safe(u'\n'.join(output))
 
+        prefix_name = name.split('-')[0]
+        output.append(u"""
+        <script>
+            (function($) {
+            $(document).ready(function() {
+                var rows = "#%s-group .inline-related";
+                var updateOrderedSelectFilter = function() {
+                    // If any SelectFilter widgets are a part of the new form,
+                    // instantiate a new SelectFilter instance for it.
+                    if (typeof OrderedSelectFilter != "undefined"){
+                        $(".sortedm2m").each(function(index, value){
+                            var namearr = value.name.split('-');
+                            OrderedSelectFilter.init(value.id, namearr[namearr.length-1], false, "%s");
+                        });
+                        $(".sortedm2mstacked").each(function(index, value){
+                            var namearr = value.name.split('-');
+                            OrderedSelectFilter.init(value.id, namearr[namearr.length-1], true, "%s");
+                        });
+                    }
+                }
+                $(rows).formset({
+                    prefix: "%s",
+                    addText: "%s %s",
+                    formCssClass: "dynamic-%s",
+                    deleteCssClass: "inline-deletelink",
+                    deleteText: "Remove",
+                    emptyCssClass: "empty-form",
+                    added: (function(row) {
+                        updateOrderedSelectFilter();
+                    })
+                });
+            });
+        })(django.jQuery)
+        </script>""" % (
+        prefix_name, settings.ADMIN_MEDIA_PREFIX, settings.ADMIN_MEDIA_PREFIX, prefix_name, _('Add another'),
+        name.split('-')[-1], prefix_name))
+
+        return mark_safe(u'\n'.join(output))
 
 
     def render_option(self, selected_choices, option_value, option_label):
@@ -62,7 +99,7 @@ class SortedFilteredSelectMultiple(forms.SelectMultiple):
         selected_html = (option_value in selected_choices) and u' selected="selected"' or ''
         try:
             index = list(selected_choices).index(escape(option_value))
-            selected_html = u'%s %s' % (u' data-sort-value="%s"' % index , selected_html)
+            selected_html = u'%s %s' % (u' data-sort-value="%s"' % index, selected_html)
         except ValueError:
             pass
 
@@ -84,14 +121,20 @@ class SortedFilteredSelectMultiple(forms.SelectMultiple):
                 output.append(self.render_option(selected_choices, option_value, option_label))
         return u'\n'.join(output)
 
-class SortedMultipleChoiceField(forms.ModelMultipleChoiceField):
+    def _has_changed(self, initial, data):
+        if initial is None:
+            initial = []
+        if data is None:
+            data = []
+        initial_list = [force_unicode(value) for value in initial]
+        return data != initial_list
 
+class SortedMultipleChoiceField(forms.ModelMultipleChoiceField):
     def __init__(self, queryset, cache_choices=False, required=True,
                  widget=None, label=None, initial=None,
                  help_text=None, *args, **kwargs):
-
         if not widget:
-            widget=SortedFilteredSelectMultiple(
+            widget = SortedFilteredSelectMultiple(
                 is_stacked=kwargs.get('is_stacked', False)
             )
         super(ModelMultipleChoiceField, self).__init__(queryset, None,
